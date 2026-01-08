146cbe28269b9fbec70774c2a78cf00859ee57c5
vitinhomerces13-sys <vitinhomerces13@gmail.com>
2026-01-08 19:49:41 +0530

    Add text overlay function to image_utils.py

--- ai_thumbnail_saas/app/image_utils.py
+++ ai_thumbnail_saas/app/image_utils.py
@@ -1,1 +1,9 @@
+from PIL import Image,ImageDraw,ImageFont
+from config import FONT_PATH,IMAGE_W,IMAGE_H
 
+def text_overlay(inp,txt,out):
+    i=Image.open(inp).resize((IMAGE_W,IMAGE_H))
+    d=ImageDraw.Draw(i)
+    f=ImageFont.truetype(FONT_PATH,90)
+    d.text((50,300),txt,font=f,fill="yellow",stroke_width=4,stroke_fill="black")
+    i.save(out)
